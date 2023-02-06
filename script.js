20f349940a4bd5e76e872dd04667bac93971ec8c
Piotrek197 <okroj.piotrek@gmail.com>
2023-02-06 22:05:53 +0100

    add selected color, remove selected color

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ const selectedColor = document.getElementById("selected-color");
 const randomColor = document.getElementById("random-color");
 const imgInp = document.getElementById("file-input");
 const canvasWrapper = document.getElementById("canvas-wrapper");
+const addColorButton = document.getElementById("add-color-button");
 
 const MAX_WIDTH = window.innerWidth * 0.5;
 const MAX_HEIGHT = window.innerHeight * 0.8;
 
 const DEFAULT_COLORS = 10;
 
-// let ctx;
+let colors = [];
+
+if (addColorButton) {
+  addColorButton.addEventListener("click", e => {
+    if (e.target.dataset.color) addColorToPallete(e.target.dataset.color);
+  });
+}
+
 imgInp.onchange = evt => {
   const [file] = imgInp.files;
   if (file) {
-    // blah.src = URL.createObjectURL(file);
-    // const canvas = document.getElementById("canvas");
-    // ctx = canvas.getContext("2d");
     deleteAllPins();
     drawImage(file);
 
@@ -40,17 +45,50 @@ function drawImage(file) {
     canvasWrapper.style.height = height;
     // console.log("image size after resizing", width, height);
     ctx.drawImage(this, 0, 0, width, height);
-    console.log("image size", this.width, this.height);
+    // console.log("image size", this.width, this.height);
     // console.log(getRandomInt(this.width), getRandomInt(this.height));
 
     img.style.display = "none";
 
     canvas.addEventListener("mousemove", event => pick(event, hoveredColor, null, ctx));
-    canvas.addEventListener("click", event => pick(event, selectedColor, null, ctx));
-    for (let i = 0; i < DEFAULT_COLORS; i++) {
-      addPin(getRandomInt(width), getRandomInt(height), ctx);
-    }
+    canvas.addEventListener("click", event => {
+      const color = pick(event, selectedColor, null, ctx);
+      addColorButton.dataset.color = color;
+      addColorButton.style.display = "inline-block";
+    });
+    displayColors(width, height, ctx);
   });
+}
+
+function displayColors(width, height, ctx) {
+  for (let i = 0; i < DEFAULT_COLORS; i++) {
+    addPin(getRandomInt(width), getRandomInt(height), ctx);
+  }
+
+  document.querySelectorAll(".color-preview").forEach(colorPreview => {
+    colorPreview.removeEventListener("click", deleteColor);
+  });
+  document.querySelectorAll(".color-preview").forEach(colorPreview => {
+    colorPreview.addEventListener("click", deleteColor);
+  });
+}
+
+function addColorToPallete(color) {
+  if (colors[colors.length - 1] !== color) {
+    const colorPreview = document.createElement("div");
+    colorPreview.className = "color-preview random-color";
+    const idx = makeid();
+    colorPreview.id = idx;
+    colorPreview.style.background = color;
+    document.getElementById("random-color-wrapper").appendChild(colorPreview);
+    colors.push({ id: idx, color });
+    document.querySelectorAll(".color-preview").forEach(colorPreview => {
+      colorPreview.removeEventListener("click", deleteColor);
+    });
+    document.querySelectorAll(".color-preview").forEach(colorPreview => {
+      colorPreview.addEventListener("click", deleteColor);
+    });
+  }
 }
 
 function getRandomInt(max) {
@@ -62,18 +100,22 @@ function addPin(x, y, ctx) {
 
   const colorPreview = document.createElement("div");
   colorPreview.className = "color-preview random-color";
-  colorPreview.id = "random-color";
+  const idx = makeid();
+  colorPreview.id = idx;
   document.getElementById("random-color-wrapper").appendChild(colorPreview);
 
   pin.className = "color-pin";
-  pin.style.left = x - 10 + "px";
-  pin.style.top = y - 10 + "px";
+  pin.style.left = x - 9 + "px";
+  pin.style.top = y - 9 + "px";
+  pin.id = "pin_" + idx;
   const rgba = pick(null, colorPreview, { x, y }, ctx);
+  colors.push({ id: idx, color: rgba });
+  // console.log("colors add pin", colors);
   colorPreview.background = rgba;
   pin.style.background = rgba;
-  console.log("pin coords", x, y);
+  // console.log("pin coords", x, y);
   document.getElementById("canvas-wrapper").appendChild(pin);
-  console.log("random c", x, y);
+  // console.log("random c", x, y);
 }
 
 function getImageXY(width, height) {
@@ -101,6 +143,13 @@ function deleteAllPins() {
   });
 }
 
+function deleteColor(e) {
+  const id = e.target.id;
+  document.getElementById(id).remove();
+  document.getElementById("pin_" + id)?.remove();
+  colors = colors.filter(color => color.id !== id);
+}
+
 function pick(event, destination, random, ctx) {
   const bounding = canvas.getBoundingClientRect();
   let x;
@@ -126,4 +175,16 @@ function pick(event, destination, random, ctx) {
     document.getElementById(`${destination.id}-value`).textContent = rgba;
 
   return rgba;
+}
+
+function makeid(length = 10) {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
